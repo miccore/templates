7664d319066fee8038d4bdb87f565322a97d858c
MbakopManuel <61746673+MbakopManuel@users.noreply.github.com>
2021-04-30 14:34:23 +0200

    template sample microservice

--- Micro-dotnet-with-auth/User.Microservice/ApiResponses/ApiError.cs
+++
@@ -1,9 +0,0 @@
-namespace User.Microservice.Operations.ApiResponses
-{
-    public class ApiError
-    {
-        public int Code { get; set; }
-
-        public string Message { get; set; }
-    }
-}
--- Micro-dotnet-with-auth/scripts/start.sh
+++
@@ -1,1 +0,0 @@
-pm2 delete User.microservice;pm2 delete gateway.webapi;dotnet build ../Microservice.WebApi.sln;pm2 start start-user.sh --name User.microservice;pm2 start start-gateway.sh --name gateway.webapi;
