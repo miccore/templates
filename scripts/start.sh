15ce5e154d22a64f1005dd99ef7ed9dca241cb73
MbakopManuel <61746673+MbakopManuel@users.noreply.github.com>
2021-05-12 20:36:51 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-pm2 delete Sample.microservice;pm2 delete gateway.webapi;dotnet build ../Microservice.WebApi.sln;pm2 start start-user.sh --name Sample.microservice;pm2 start start-gateway.sh --name gateway.webapi;
+pm2 delete gateway.webapi;dotnet build ../Microservice.WebApi.sln;pm2 start start-gateway.sh --name gateway.webapi;
