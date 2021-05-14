2e8b4619dd1c13e81d6a5bfb450504e12c025c2c
MbakopManuel <61746673+MbakopManuel@users.noreply.github.com>
2021-05-14 18:16:15 +0200

    Update UserViewModel.cs

--- Operations/Users/ViewModels/UserViewModel.cs
+++ Operations/Users/ViewModels/UserViewModel.cs
@@ -14,7 +14,6 @@ namespace User.Microservice.Operations.User.ViewModels {
         public string Email { get; set; }
         public string RefreshToken { get; set; }
         public string Address { get; set; }
-        public int Created_at { get; set;}
         public string? Token {get; set;}
         public RoleViewModel Role {get; set;}
     }
