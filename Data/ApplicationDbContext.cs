3fce14d4c830872ea64ddf8057c4d63c6b77641e
MbakopManuel <61746673+MbakopManuel@users.noreply.github.com>
2021-05-14 13:39:13 +0200

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
+/** Begin Import */
 
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using User.Microservice.Repositories.User.DtoModels;
-using User.Microservice.Repositories.Role.DtoModels;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using User.Microservice.Repositories.User.DtoModels;
+    using User.Microservice.Repositories.Role.DtoModels;
+
+/* End Import */
 
 namespace User.Microservice.Data
 {
