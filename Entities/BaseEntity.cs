4623fba76f31e56d760ad4903575559f773cd8ba
MbakopManuel <61746673+MbakopManuel@users.noreply.github.com>
2021-05-14 17:36:06 +0200

    Update BaseEntity.cs

--- Entities/BaseEntity.cs
+++ Entities/BaseEntity.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace Sample.Microservice.Entities
 {
-public abstract class BaseEntity
-{
-    public int? Id { get; set; }
-}
+    public abstract class BaseEntity
+    {
+        public int? Id { get; set; }
+        
+        public int? CreatedAt { get; set;}
+        
+        public int? UpdatedAt { get; set;}
+        
+        public int? DeletedAt { get; set;}
+    }
 }
