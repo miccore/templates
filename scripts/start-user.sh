32ec07062fbedb580ff84bc03941d194607ec717
Manuel Mbakop <mbakopmanuel1@gmail.com>
2021-05-24 10:13:20 +0100

    update database at startup

--- scripts/start-user.sh
+++
@@ -1,1 +0,0 @@
-dotnet restore ../User.Microservice/User.Microservice.csproj;dotnet publish ../User.Microservice/User.Microservice.csproj -c Release;dotnet ../User.Microservice/bin/Release/netcoreapp3.1/User.Microservice.dll --urls "http://localhost:44373";
