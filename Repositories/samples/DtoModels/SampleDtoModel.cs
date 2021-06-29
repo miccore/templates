c76901252a60dc066dd6947212c15de109462ff7
MbakopManuel <61746673+MbakopManuel@users.noreply.github.com>
2021-06-29 15:44:52 +0200

    Update SampleDtoModel.cs

--- Repositories/samples/DtoModels/SampleDtoModel.cs
+++ Repositories/samples/DtoModels/SampleDtoModel.cs
@@ -1,8 +1,11 @@
 using System;
 using Sample.Microservice.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 
 namespace Sample.Microservice.Repositories.Sample.DtoModels {
-
+    [Table("Samples")]
     public class SampleDtoModel : BaseEntity {
        
         public string Name { get; set; }
