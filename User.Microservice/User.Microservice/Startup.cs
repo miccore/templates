91fec3ba04625872d559449ea70a9c2459629383
MbakopManuel <61746673+MbakopManuel@users.noreply.github.com>
2021-09-30 01:38:42 +0100

    Update Startup.cs

--- User.Microservice/User.Microservice/Startup.cs
+++ User.Microservice/User.Microservice/Startup.cs
@@ -168,8 +168,6 @@ namespace User.Microservice
                 endpoints.MapControllers();
             });
 
-            // UpdateDatabase(app);
-
         }
 
         private static void UpdateDatabase(IApplicationBuilder app){
