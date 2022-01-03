81a2d19fcf1014a65d81ed63515836500dda6e99
Manuel Mbakop <mbakopmanuel1@gmail.com>
2022-01-03 21:32:02 +0100

    git ignore and namespace

--- Gateway.WebApi/Program.cs
+++ Gateway.WebApi/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Gateway.WebApi
+namespace Miccore.Net.webapi_template.Gateway.WebApi
 {
     public class Program
     {
