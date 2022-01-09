3e19bf24fc8c730df25ca24c74eada51e0698ee8
Mbakop Manuel <mbakopmanuel@hotmail.com>
2022-01-09 12:55:42 +0100

    packages update

--- Sample.Api/Startup.cs
+++ Sample.Api/Startup.cs
@@ -46,7 +46,8 @@ namespace Miccore.Net.webapi_template.Sample.Api
 
             CorsConfiguration(services);
             services.AddDbContextPool<ApplicationDbContext>(
-                    options => options.UseMySql($"server={host};port={port};database={db};user={user};password={password}" 
+                    options => options.UseMySql($"server={host};port={port};database={db};user={user};password={password}",
+                    new MySqlServerVersion(new Version())
              ));
 
             services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
