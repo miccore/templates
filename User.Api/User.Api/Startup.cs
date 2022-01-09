5f433e9a10553475c73a9a0702df9ac58151b167
Mbakop Manuel <mbakopmanuel@hotmail.com>
2022-01-09 12:43:24 +0100

    packages update

--- User.Api/User.Api/Startup.cs
+++ User.Api/User.Api/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
