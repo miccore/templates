4609e100b0b9e02590669c06e383fac01c858a11
Mbakop Manuel <mbakopmanuel@hotmail.com>
2022-01-25 15:27:17 +0100

    upgrade to dotnet core 6

--- User.Microservice/User.Microservice/Startup.cs
+++ User.Microservice/User.Microservice/Startup.cs
@@ -56,6 +56,7 @@ namespace User.Microservice
 
             //services and repository
             addServive(services);
+            
 
             UserServiceCollectionExtensions.AddAutoMapper(services, GetAssemblyNamesToScanForMapperProfiles());
 
